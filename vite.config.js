9b1433a2c2806ddbeb028a9a27da2c75df9ab2aa
Monde Sineke <nsineke@gmail.com>
2023-04-20 13:21:07 +0200

    init(*): 🎉 Create Vite
    Setup project

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+import { resolve } from "path";
+import { defineConfig } from "vite";
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        // ADD YOUR PAGES HERE
+      },
+    },
+  },
+});
